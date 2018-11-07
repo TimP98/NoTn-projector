9f6841b1b8a13a4a3267ea4f1906a1df53e8c208
TimP98 <tim.pieters@student.fontys.nl>
2018-11-07 12:24:28 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once ('start.php'); ?>
+    <?php require_once (ASSET_ROOT . '/snippets/headlinks.php'); ?>
+</head>
+<body id="page-wrapper">
+    <?php require_once (BACK_ROOT . '/file.php'); ?>
+    <div id="content"></div>
+</body>
+</html>
---
+++ start.php
@@ -0,0 +1,10 @@
+<?php
+    ini_set('display_errors', 'On');
+
+    define('APP_ROOT', __DIR__);
+    define('VIEW_ROOT', APP_ROOT . '/view');
+    define('ASSET_ROOT', APP_ROOT . '/assets');
+    define('BACK_ROOT', APP_ROOT . '/backend');
+    define('BASE_URL', 'http://localhost/slider');
+
+?>
